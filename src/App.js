7da0c5506a179e1b803d7e5749bf667a9cb05d15
yangjian <yangj0412@foxmail.com>
2020-03-31 01:46:57 +0800

    fix: add code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,107 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useCallback, useState, useRef} from 'react';
+import './App.css'
+
+let idSeq = Date.now()
+
+function Control(props) {
+    const inputRef = useRef(null)
+    const { addTodo } = props
+
+    const onSubmit =(e) => {
+        e.preventDefault()
+        const newText = inputRef.current.vlaue.trim()
+        if (newText.length === 0) {
+            return
+        }
+        addTodo({
+            id: ++idSeq,
+            text: newText,
+            complete: false
+        })
+
+        inputRef.current.value =''
+    }
+    return <div className="control">
+        <h1>todos</h1>
+        <form onSubmit={onSubmit}>
+            <input type='text' ref={inputRef} className="new-todo" placeholder="new thing to be done"/>
+        </form>
+    </div>
+}
+function TodoItem(props) {
+    const {
+        todo: {
+            id,
+            text,
+            complete
+        },
+        toggleTodo,
+        removeTodo
+    } = props
+    const onChange = () => {
+        toggleTodo(id)
+    }
+    const onRemove = () => {
+        removeTodo(id)
+    }
+
+    return (
+        <li className="todo-item">
+            <input type="checkbox" onChange={onChange} checked={complete}/>
+            <label className={complete ? 'complete': ''}>{text}</label>
+            <button onClick={onRemove}>&#xd7;</button>
+        </li>
+    )
+}
+function Todos(props) {
+    const { todos, toggleTodo, removeTodo } = props
+    return (
+        <ul>
+            {
+                todos.map(todo => {
+                    return (
+                        <TodoItem />
+                    )
+                })
+            }
+        </ul>
+    )
+}
+
+function TodoList() {
+    const [todos, setTodos] = useState([])
+    const addTodo = useCallback((todo) => {
+        setTodos(todos => [...todos, todo])
+    }, [])
+    const removeTodo = useCallback((id) => {
+        setTodos(todos => todos.filter(todo => {
+            return todo.id !== id
+        }))
+    }, [])
+
+    const toggleTodo = useCallback((id) => {
+        setTodos(todos => todos.map(todo => {
+            return todo.id === id
+                ? {
+                    ...todo,
+                    complete: !todo.complete
+                }
+                : todo
+        }))
+    }, [])
+
+  return (
+      <div className="todo-list">
+        <Control addTodo={addTodo}/>
+        <Todos removeTodo={removeTodo} toggleTodo={toggleTodo} todos={todos}/>
+      </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <TodoList/>
     </div>
   );
 }
